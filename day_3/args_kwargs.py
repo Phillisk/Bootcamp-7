c5f11e1eeced855d68bde9b3175f0b9b313d21c3
Phillis Kiragu <kiragu.w.phillis@gmail.com>
2016-04-27 14:44:50 +0300

    changes with args and kwargs

--- day_3/args_kwargs.py
+++ day_3/args_kwargs.py
@@ -20,3 +20,33 @@ people = [
 
 for person in people:
 	print hello(*person)
+
+def super_sum(*args):
+	'''
+	Takes in variable number of items,
+	and returns the sum.
+	e.g. super_sum(10, 20) = 30
+		super_sum(10, 20, 40) = 70
+		super_sum(1, 2, 3, 4, 5) = 15
+	'''
+	total = 0
+	for i in args:
+		total += i
+
+	return total
+
+print super_sum(10, 20)
+print super_sum(1, 4, 7, 5)
+a = [10, 40, 60]
+print super_sum(*a)
+
+
+def hello_again(**kwargs):
+	return "I am {}, and I'm {} y/o".format(kwargs['name'], kwargs['age'])
+
+print hello_again(name='Joe', age=20)
+print hello_again(age=20, name='Jane')
+
+joe = {'name': 'Joe', 'age': 28}
+print hello_again(**joe)
+print hello_again(name='joe', age = 28)
